8c907cd083f9ee5f8ff404ebb89e1fcea3b945e6
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2023-02-20 17:59:39 -0300

    49: clases y arrays

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,28 +1,69 @@
-let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
-let ataquesDelJugador = document.getElementById("ataques-del-jugador")
-
-let botonMascotaJugador = document.getElementById("boton-mascota");
-let botonFuego = document.getElementById("boton-fuego");
-let botonAgua = document.getElementById("boton-agua");
-let botonTierra = document.getElementById("boton-tierra");
-let botonReiniciar = document.getElementById("boton-reiniciar")
-
-let inputHipodoge = document.getElementById("hipodoge");
-let inputCapipepo = document.getElementById("capipepo");
-let inputRatigueya = document.getElementById("ratigueya");
-
-let sectionMensajes = document.getElementById("resultado")
-// let sectionMensajes = document.getElementById("resultado")d
-let sectionReiniciar = document.getElementById("reiniciar")
-let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
-
-let spanMascotaJugador = document.getElementById("mascota-jugador");
-let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
-let spanVidasJugador = document.getElementById("vidas-jugador")
-let spanVidasEnemigo = document.getElementById("vidas-enemigo")
+const ataquesDelJugador = document.getElementById("ataques-del-jugador")
+const ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
+const botonMascotaJugador = document.getElementById("boton-mascota");
+const botonFuego = document.getElementById("boton-fuego");
+const botonAgua = document.getElementById("boton-agua");
+const botonTierra = document.getElementById("boton-tierra");
+const botonReiniciar = document.getElementById("boton-reiniciar")
+const inputHipodoge = document.getElementById("hipodoge");
+const inputCapipepo = document.getElementById("capipepo");
+const inputRatigueya = document.getElementById("ratigueya");
+const sectionMensajes = document.getElementById("resultado")
+const sectionReiniciar = document.getElementById("reiniciar")
+const sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+const spanMascotaJugador = document.getElementById("mascota-jugador");
+const spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+const spanVidasJugador = document.getElementById("vidas-jugador")
+const spanVidasEnemigo = document.getElementById("vidas-enemigo")
 
 let ataqueJugador
 let ataqueEnemigo 
+// COMBATE
+let resultadoCombate = "";
+let vidasJugador = 3
+let vidasEnemigo = 3
+let mokepones = []
+
+class Mokepon {
+    constructor(nombre, foto, vida) {
+        this.nombre = nombre;
+        this.foto = foto;
+        this.vida = vida;
+        this.ataques = []
+    }
+}
+
+
+let hipodoge = new Mokepon("Hipodogue", "./img/hipodoge.png", 5)
+let capipepo = new Mokepon("Capipepo", "./img/capipepo.png", 5)
+let ratigueya = new Mokepon("Ratigueya", "./img/ratigueya.png", 5)
+
+mokepones.push(hipodoge,capipepo,ratigueya)
+
+hipodoge.ataques.push (
+    { nombre: '♒', id:"boton-agua" },
+    { nombre: '♒', id:"boton-agua" },
+    { nombre: '♒', id:"boton-agua" },
+    { nombre: '🔥', id:"boton-fuego" },
+    { nombre: '🔥', id:"boton-fuego" }
+) 
+
+capipepo.ataques.push (
+    { nombre: '🌱', id:"boton-tierra" },
+    { nombre: '🌱', id:"boton-tierra" },
+    { nombre: '🌱', id:"boton-tierra" },
+    { nombre: '♒', id:"boton-agua" },
+    { nombre: '🔥', id:"boton-fuego" }
+) 
+
+ratigueya.ataques.push (
+    { nombre: '🔥', id:"boton-fuego" },
+    { nombre: '🔥', id:"boton-fuego" },
+    { nombre: '🔥', id:"boton-fuego" },
+    { nombre: '♒', id:"boton-agua" },
+    { nombre: '🌱', id:"boton-tierra" }
+) 
+
 
 
 
@@ -174,10 +215,7 @@ function seleccionarMascotaJugador() {
  }
 
 
-// COMBATE
-let resultadoCombate = "";
-let vidasJugador = 3
-let vidasEnemigo = 3
+
 
 function combate() {
     

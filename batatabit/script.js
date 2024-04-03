6cf88334e5470c3768a4da9b3649e6c91f56233c
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2024-04-03 19:27:58 -0300

    Batata-Bit -> Info-cards

--- batatabit/script.js
+++ batatabit/script.js
@@ -6,13 +6,11 @@ const comission = document.getElementById("comission")
 
 
 right_arrow.onclick = function() {
-//    event.preventDefault();
    coins.style.display     = "none"
    comission.style.display = "block"
 }
 
 left_arrow.onclick = function() {
-    // event.preventDefault();
     coins.style.display     = "block"
     comission.style.display = "none"
 }

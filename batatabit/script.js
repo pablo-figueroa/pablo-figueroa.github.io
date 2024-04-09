154431f82801180ac35f2a64625cc1657f4bef75
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2024-04-08 22:53:34 -0300

    Batata-Bit -> planes - scrollbar

--- batatabit/script.js
+++ batatabit/script.js
@@ -14,3 +14,25 @@ left_arrow.onclick = function() {
     coins.style.display     = "block"
     comission.style.display = "none"
 }
+
+// ********************************************************
+
+document.addEventListener("DOMContentLoaded", function() {
+  const escrol = document.getElementById("escrol");
+  const escrol_izq = document.getElementById("escrol_izq");
+const escrol_der = document.getElementById("escrol_der");
+
+  function checkVisibility() {
+    if (escrol.scrollWidth > escrol.clientWidth) {
+      escrol_izq.style.visibility = "visible";
+      escrol_der.style.visibility = "visible";
+    } else {
+      escrol_izq.style.visibility = "hidden";
+      escrol_der.style.visibility = "hidden";
+    }
+  }
+
+  checkVisibility();
+
+  window.addEventListener("resize", checkVisibility);
+ });

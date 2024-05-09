4481b0fd62dd67141579f61f20a35c5ad000eec2
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2024-05-08 21:55:46 -0300

    platzi-travel -> hola tailwind

---
+++ platzi-travel/tailwind.config.js
@@ -0,0 +1,66 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    darkMode: true,
+    darkMode: "class",
+    darkMode: {
+      // Base styles for dark mode
+      backgroundColor: "gray-900",
+      textColor: "white",
+      borderColor: "gray-700"
+      // ... other dark mode specific styles
+    },
+    extend: {
+      colors: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9"
+      },
+      backgroundImage: {
+        losangeles:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/LA.jpg?raw=true')",
+        bali:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/bali.jpg?raw=true')",
+        chicago:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/chicago.jpg?raw=true')",
+        europe:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/europe.jpg?raw=true')",
+        iceland:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/iceland.jpg?raw=true')",
+        miami:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/miami.jpg?raw=true')",
+        new_york:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/new_york.jpg?raw=true')",
+        norway:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/norway.jpg?raw=true')",
+        sanFrancisco:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/sanFrancisco.jpg?raw=true')",
+        sanFranciscoDesktop:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/sanFranciscoDesktop.jpg?raw=true')",
+        seattle:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/seattle.jpg?raw=true')",
+        switzerland:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/switzerland.jpg?raw=true')",
+        sydney:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/sydney.jpg?raw=true')",
+        yosemite:
+          "url('https://github.com/platzi/PlatziTravel/blob/main/public/img/yosemite.jpg?raw=true')"
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat"]
+      }
+    }
+  },
+  variants: {
+    extend: {
+      with: ["responsive", "hover", "focus"]
+    }
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography")
+  ],
+};
